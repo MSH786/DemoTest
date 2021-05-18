7cdf5ff2f65d6278b00558b609a64bc8faaa4011
MSH786 <58289501+MSH786@users.noreply.github.com>
2021-05-18 22:09:18 +0500

    Add Demo Project Files, Check
    There is all files added,
    in component folder there is a 3 files, 
    
    Login, 
    Signup
    Home
    
    these all are main files used in project Design.
    
    I work according to design point of view just.
    
    All files import into app.js
    
    Please check all.

---
+++ App.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
